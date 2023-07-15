9d12fef3f2984e7737f9c152798c62916fd905a9
@十三 <1714037953@qq.com>
2023-07-15 22:46:17 +0800

    网页标题

--- 1.0.0/index.js
+++ 1.0.0/index.js
@@ -1,5 +1,65 @@
+// 渲染header
+function $header(nowMenu) {
+    let menuList = [
+        {
+            id: 0,
+            name: "指南",
+            page: ""
+        },
+        {
+            id: 1,
+            name: "组件",
+            page: "component"
+        },
+        {
+            id: 2,
+            name: "资源",
+            page: ""
+        },
+    ]
+    // 创建 header
+    let header = document.getElementById("header")
+    // 获取 header-container 容器
+    let headerContainer = document.createElement("div")
+    headerContainer.classList.add("header-container")
+    // 创建 header logo
+    let headerContainerLogo = document.createElement("div")
+    headerContainerLogo.classList.add("header-container-logo")
+    let headerLogoIcon = document.createElement("img")
+    headerLogoIcon.src = "../../logo.png"
+    let headerLogoName = document.createElement("span")
+    headerLogoName.innerHTML = "小猿组件库"
+    // 创建 header 菜单
+    let headerContainerMenus = document.createElement("header-container-menus")
+    headerContainerMenus.classList.add("header-container-menus")
+    // 向 header 插入元素
+    header.appendChild(headerContainer)
+    headerContainer.appendChild(headerContainerLogo)
+    headerContainerLogo.appendChild(headerLogoIcon)
+    headerContainerLogo.appendChild(headerLogoName)
+    headerContainer.appendChild(headerContainerMenus)
+    for (let i = 0; i < menuList.length; i++) {
+        let menu = document.createElement("div")
+        menu.classList.add("header-container-menu")
+        let menuName = document.createElement("div")
+        menuName.classList.add("header-container-menu-name")
+        menuName.innerHTML = menuList[i].name
+        let menuBorder = document.createElement("div")
+        menuBorder.classList.add("header-container-menu-border")
+        menuBorder.innerHTML = menuList[i].name
+        if (nowMenu == menuList[i].name) {
+            menu.style.opacity = 1
+            menuBorder.style.opacity = 1
+        }
+        // 插入菜单
+        headerContainerMenus.appendChild(menu)
+        menu.appendChild(menuName)
+        menu.appendChild(menuBorder)
+    }
+
+}
 // 渲染选项卡
-function $a(component) {
+function $compentTabs(component) {
     let tabList = [
         {
             type: "title",
@@ -93,6 +153,7 @@ function $a(component) {
         }
     }
 }
+// 选项卡动画
 function $animation() {
     let tabs = document.getElementById("tabsBox")
     let content = document.getElementById("content")
